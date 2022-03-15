09039d9311417d1d122a4ddd0d85d8603248e1de
César Galindo <33326122+chestergalindo@users.noreply.github.com>
2022-03-15 17:14:55 -0500

    feat [develop]: add first version of point two

--- src/taskTwo.js
+++ src/taskTwo.js
@@ -1,1 +1,12 @@
-
+function sum ( a )
+{
+  let defaultValue = 0;
+  switch ( typeof a )
+  {
+    case 'function':
+      return totalValue = a( defaultValue );
+    case 'number':
+      defaultValue = a + defaultValue;
+      return ( () => sum( a ) );
+  }
+}
